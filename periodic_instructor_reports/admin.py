b14fa9fc2a18632a2fd46e84cadf3392854e3723
evhenyj.sahyrov <evhenyj.sahyrov@raccoongang.com>
2022-03-04 12:14:52 +0000

    CAMPUS-897 [Blender reports] problem grade report that runs every minute doesn’t work

--- periodic_instructor_reports/admin.py
+++ periodic_instructor_reports/admin.py
@@ -26,13 +26,6 @@ class PeriodicReportScheduleAdmin(admin.ModelAdmin):
     Django admin widget for `PeriodicReportSchedule`s.
     """
 
-    list_display = ["task", "interval", "courses", "arguments", "keyword_arguments"]
-    list_filter = ["task__name", "course_ids"]
+    list_display = ["task", "interval", "course_ids", "arguments", "keyword_arguments"]
+    list_filter = ["task__name"]
     search_fields = ["task__name", "task__path", "course_ids"]
-
-    def courses(self, obj: PeriodicReportSchedule) -> str:
-        """
-        Return the formatted list of courses.
-        """
-
-        return ", ".join(list(obj.course_ids))
